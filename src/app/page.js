0b4c054399a8005ba9a33e4297801febf65ee0f9
Dhairya Bhardwaj <dhairya.bhardwaj@albabank.co.uk>
2025-01-11 14:04:54 +0000

    refactoring the YAML structure to allow more flexibility of defining sections

--- src/app/page.js
+++ src/app/page.js
@@ -2,61 +2,26 @@
 import fs from 'fs';
 import path from 'path';
 import YAML from 'yaml';
-import { DoubleColumnList, ListItem, Section, SubSection, ResumeContainer, ContactsContainer, ResumeTitle, ItemList} from './styled-comps';
-import { parseMarkdown } from './pares-markdown';
-import { PhoneContactEntry, LinkedinContactEntry, EmailContactEntry, GithubContactEntry } from './contact-comps'
+import { Resume } from './cv';
 
 const Home = () => {
 
-  const filePath = path.join(process.cwd(), '/src/app/data.yaml');
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  let data = YAML.parse(fileContents);
+  let data = loadYamlToMap('cv.yaml')
+  let cv = data.get('cv');
+  let config = data.get('config');
+  let defaultConfig = loadYamlToMap('config.yaml');
+
+  //TODO: merge config from the yaml file to the default config
 
   return (
-    <ResumeContainer>
-      <ResumeTitle title={data.personal.name} />
-      <ContactsContainer>
-        <PhoneContactEntry phone={data.personal.phone} />
-        <EmailContactEntry email={data.personal.email} />
-        <LinkedinContactEntry linkedin={data.personal.linkedin} />
-        <GithubContactEntry github={data.personal.github} />
-      </ContactsContainer>
-
-      <Section heading="Experience">
-        {data.experience.map((job, index) => (
-          <SubSection index={index} title={job.company} subtitle={job.position} rhsTop={parseMarkdown(job.duration)} rhsDown={job.location}>
-            <ItemList items={job.responsibilities} />
-          </SubSection>
-        ))}
-      </Section>
-
-      <Section heading="Technical Skills">
-        <DoubleColumnList>
-          {data.technical_skills.map((skill, index) => (
-            <ListItem key={index}><span className='capitalize font-bold'>{Object.keys(skill)[0]}:</span> {Object.values(skill)[0]}</ListItem>
-          ))}
-        </DoubleColumnList>
-      </Section>
-
-      <Section heading="Education">
-        {data.education.map((edu, index) => (
-          <SubSection index={index} title={edu.institution} subtitle={`${edu.degree}, CGPA: ${edu.cgpa}`} rhsTop={edu.year} rhsDown={edu.ExperienceItemlocation} />
-        ))}
-      </Section>
-
-      <Section heading="Projects">
-        {data.projects.map((project, index) => (
-          <SubSection index={index} title={project.name}>
-            <ItemList items={project.description} />
-          </SubSection>
-        ))}
-      </Section>
-
-      <Section heading="Miscellaneous">
-        <ItemList items={data.miscellaneous} />
-      </Section>
-    </ResumeContainer>
-  );
+    <Resume cv={cv} config={defaultConfig}></Resume>
+  )
 };
+
+function loadYamlToMap(fileName) {
+  const filePath = path.join(process.cwd(), `/data/${fileName}`);
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  return YAML.parse(fileContents, { mapAsMap: true });
+}
 
 export default Home;
