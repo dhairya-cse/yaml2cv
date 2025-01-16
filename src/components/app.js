e857d84db4f0b7c80cffd3532a91c8260893a086
Dhairya Bhardwaj <dhairya.bhardwaj@albabank.co.uk>
2025-01-16 20:33:00 +0000

    Adding some logic for allowing editing to logged in user

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import { Resume } from "./cv"
 import YamlEditor from "./yaml-editor"
 import { useEffect, useState } from "react";
 
-export function App({ cvYaml, configYaml }) {
+export function App({ cvYaml, configYaml, loggedIn, canEdit }) {
     const [yamlContent, setYamlContent] = useState(cvYaml);
     const [resume, setResume] = useState();
     const [error, setError] = useState(null);
@@ -32,12 +32,24 @@ export function App({ cvYaml, configYaml }) {
         setYamlContent(value);
     };
 
-    return <div className="flex h-screen overflow-hidden print:contents">
-        <div className="flex-1 h-full overflow-y-auto bg-white border-r-slate-300 border-r-2 border-black print:hidden">
-            <YamlEditor value={yamlContent} onChange={handleEditorChange} />
+    if (canEdit) {
+        return <div className="flex h-screen overflow-hidden print:contents">
+            <div className="flex-1 h-full overflow-y-auto bg-white border-r-slate-300 border-r-2 border-black print:hidden">
+                <YamlEditor value={yamlContent} onChange={handleEditorChange} />
+            </div>
+            <ResumeWithContainer resume={resume} />
         </div>
-        <div className="h-full overflow-y-auto bg-white print:contents" >
-            {resume}
-        </div>
+    } else {
+        return resume;
+    }
+}
+
+function EditorWithContainer({ yamlContent, handleEditorChange }) {
+    return
+}
+
+function ResumeWithContainer({ resume }) {
+    return <div className="h-full overflow-y-auto bg-white print:contents" >
+        {resume}
     </div>
 }
