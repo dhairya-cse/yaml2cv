7ae67724d2e3da085a01fefc4f9fd7d44919ff8d
Dhairya Bhardwaj <dhairya.bhardwaj@albabank.co.uk>
2025-02-03 20:38:27 +0000

    changing redirect logic to runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    redirects: async () => {
-        return [
-            {
-                source: '/',
-                destination: `/${process.env.DEFAULT_PROFILE}`,
-                permanent: true,
-            },
-        ];
-    },
+    
 };
 
 export default nextConfig;
